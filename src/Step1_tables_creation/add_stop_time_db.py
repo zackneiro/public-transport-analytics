5abf593140decdb21d429d754bb9ade503fb53c9
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2025-07-29 07:43:13 +0000

    Improve code's style and clarity in add_stop_time_db.py

--- src/Step1_tables_creation/add_stop_time_db.py
+++ src/Step1_tables_creation/add_stop_time_db.py
@@ -1,57 +1,38 @@
 import sqlite3 as sql
 from sqlite3 import Connection, Cursor
 
-
-def main() -> None:
-    # read csv file to understand the base of schema.
-    """
-    table: DataFrame =
-        pd.read_csv(
-        "/workspaces/public-transport-analytics/required_data/data/gtfs/stop_times.txt",
-                        nrows=0)
-    """
-    # check the types of the rows and write a schema.
-    # print(table.dtypes)
-    """
+DB_PATH = "/workspaces/public-transport-analytics/gtfs.db"
+TABLE_SCHEMA = """
+CREATE TABLE IF NOT EXISTS stops_time(
     trip_id TEXT,
     stop_id TEXT,
     arrival_time TEXT,
-    departure time TEXT,
+    departure_time TEXT,
     stop_sequence INTEGER,
-    PRIMARY KEY (trip_id, stop_sequence)
-    """
+    PRIMARY KEY(trip_id, stop_sequence));
+"""
 
-    # create connection with the data base.
-    connection: Connection = sql.connect(
-        "/workspaces/public-transport-analytics/gtfs.db"
-    )
 
-    # create cursor.
+def main() -> None:
+    """Create the stops_time table in the GTFS SQLite3 database."""
+
+    # Connect to the GTFS database
+    connection: Connection = sql.connect(DB_PATH)
     cursor: Cursor = connection.cursor()
 
-    # create the table.
-    cursor.execute(
-        """CREATE TABLE IF NOT EXISTS stops_time(
-                   trip_id TEXT,
-                   stop_id TEXT,
-                   arrival_time TEXT,
-                   departure_time TEXT,
-                   stop_sequence INTEGER,
-                   PRIMARY KEY(trip_id, stop_sequence));"""
-    )
-
-    # commit
+    # Excucte table creation
+    cursor.execute(TABLE_SCHEMA)
     connection.commit()
 
-    # check for existence
+    # Verfiy that the tables exists
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
     print(cursor.fetchall())
 
-    # check the schema
+    # Check the correctness of schema
     cursor.execute("PRAGMA table_info(stops_time);")
     print(cursor.fetchall())
 
-    # close connection.
+    # Close the connection.
     connection.close()
 
 
