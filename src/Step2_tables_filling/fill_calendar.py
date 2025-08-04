8e01d1ab6a88a40952f8084baa708757701b18f7
Zakhar Adeev <153710848+zackneiro@users.noreply.github.com>
2025-08-04 15:36:37 +0000

    Fix style of fill_calendar.py

--- src/Step2_tables_filling/fill_calendar.py
+++ src/Step2_tables_filling/fill_calendar.py
@@ -1,25 +1,37 @@
 import sqlite3 as sql
 from sqlite3 import Connection, Cursor
-from typing import List, Tuple
+from typing import Final, List, Tuple
 
 import pandas as pd
 from pandas import DataFrame
 
+DB_PATH: Final[str] = "/workspaces/public-transport-analytics/gtfs.db"
+CALENDAR_CSV_PATH: Final[
+    str
+] = "/workspaces/public-transport-analytics/required_data/data/gtfs/calendar.txt"
+
+INSERT_INTO_CALENDAR_SQL: Final[
+    str
+] = """
+INSERT OR IGNORE INTO calendar (
+    service_id,
+    monday, tuesday, wednesday,
+    thursday, friday, saturday,
+    sunday, start_date, end_date
+) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+"""
+
 
 def main() -> None:
-    """
-    Function fills the "calendar" table in the SQL database.
-    """
-    # create connection.
-    db_connection: Connection = sql.connect(
-        "/workspaces/public-transport-analytics/gtfs.db"
-    )
-    # create cursor.
-    db_cursor: Cursor = db_connection.cursor()
-    # create dataframe and save the data to it.
+    """Populate the GTFS `calendar` table from calendar.txt."""
+
+    # Connect to the SQLite database and create a cursor
+    conn: Connection = sql.connect(DB_PATH)
+    cur: Cursor = conn.cursor()
+
+    # Load calendar.txt into a DataFrame with selected columns
     table_df: DataFrame = pd.read_csv(
-        "/workspaces/public-transport-analytics/"
-        "required_data/data/gtfs/calendar.txt",
+        CALENDAR_CSV_PATH,
         usecols=[
             "service_id",
             "monday",
@@ -33,26 +45,21 @@ def main() -> None:
             "end_date",
         ],
     )
-    # create a query.
-    insert_sql: str = """INSERT OR IGNORE INTO calendar(
-                    service_id,
-                    monday, tuesday, wednesday,
-                    thursday, friday, saturday,
-                    sunday, start_date, end_date)
-                    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);"""
-    # create rows.
+
+    # Create rows
     rows: List[Tuple[str, int, int, int, int, int, int, int, str]] = list(
         table_df.itertuples(index=False, name=None)
     )
-    # send query with rows (executemany).
-    db_cursor.executemany(insert_sql, rows)
-    # commit changes
-    db_connection.commit()
-    # check table for loadaed files.
-    db_cursor.execute("SELECT COUNT(*) FROM calendar;")
-    print("Loaded files: ", db_cursor.fetchone()[0])
+    # Insert rows into the calendar table
+    cur.executemany(INSERT_INTO_CALENDAR_SQL, rows)
+    conn.commit()
 
-    db_connection.close()
+    # Verify the number of inserted rows
+    cur.execute("SELECT COUNT(*) FROM calendar;")
+    print("Loaded files:", cur.fetchone()[0])
+
+    # Close connection
+    conn.close()
 
 
 if __name__ == "__main__":
